bba1254185be75d38c96f1cbfdaa852970deec6d
alexpachvtb <alexpachvtb@gmail.com>
2021-09-15 11:32:35 +0300

    feat: transform-array done

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,34 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  let newArr = [];
+  if (!(Array.isArray(arr))) {
+    throw new Error("\'arr\' parameter must be an instance of the Array!");
+  }
+  if (arr.length == 0) {
+    return arr;
+  }
+  for (let i = 0; i < arr.length; i ++) {
+    newArr.push(arr[i]);
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (newArr[i] != undefined) {
+      if (newArr[i] == '--discard-next') {
+        newArr[i] = undefined;
+        newArr[i + 1] = undefined;
+      }
+      if (newArr[i] == '--discard-prev') {
+        newArr[i] = undefined; 
+        newArr[i - 1] = undefined;
+      }
+      if (newArr[i] == '--double-next') {
+        newArr[i] = newArr[i + 1];
+      }
+      if (newArr[i] == '--double-prev') {
+        newArr[i] = newArr[i - 1];
+      }
+    }
+  }
+  return newArr.filter(item => item != undefined);
 }
